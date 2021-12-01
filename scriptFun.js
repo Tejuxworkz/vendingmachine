e26740a115a542ce649cad248b85a87ef0912300
Tejuxworkz <91607322+Tejuxworkz@users.noreply.github.com>
2021-12-01 23:31:22 +0530

    committing vendingmachine files

---
+++ scriptFun.js
@@ -0,0 +1,54 @@
+
+	
+	function printProduct(name) {
+		if (name == "areca-palm") {
+			var cost=70;
+			var noOfPots=document.getElementById("name").value;	
+			var price=noOfPots*cost;   
+			document.getElementById("print").innerHTML=price;
+		}
+		if (name == "azalea-flower") {
+			
+			var cost1=100;
+			var noOfPots1=document.getElementById("name1").value;
+			var price1=noOfPots1*cost1;
+			document.getElementById("print1").innerHTML=price1;
+		}
+		if (name == "jojoba-plant") {
+			var cost2=100;
+			var noOfPots2=document.getElementById("name2").value;
+			var price2=noOfPots2*cost2;
+			document.getElementById("print2").innerHTML=price2;
+		}
+		if (name == "jojoba-plant") {
+			var cost3=100;
+			var noOfPots3=document.getElementById("name3").value;
+			var price3=noOfPots3*cost3;
+			document.getElementById("print3").innerHTML=price3;
+		}
+		if (name == "lipstick-plant") {
+			var cost4=100;
+			var noOfPots4=document.getElementById("name4").value;
+			var price4=noOfPots4*cost4;
+			document.getElementById("print4").innerHTML=price4;
+		}
+		if (name == "lucky-bamboo-plants") {
+			var cost5=100;
+			var noOfPots5=document.getElementById("name5").value;
+			var price4=noOfPots4*cost5;
+			document.getElementById("print5").innerHTML=price5;
+		}
+		if (name == "purple-waffle-plant") {
+			var cost6=100;
+			var noOfPots6=document.getElementById("name6").value;
+			var price6=noOfPots6*cost6;
+			document.getElementById("print6").innerHTML=price6;
+		}
+		if (name == "syngonium-podophyllum") {
+			var cost7=100;
+			var noOfPots7=document.getElementById("name7").value;
+			var price7=noOfPots7*cost7;
+			document.getElementById("print7").innerHTML=price7;
+		}
+
+	}
